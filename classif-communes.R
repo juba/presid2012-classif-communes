e88b3c21575ccc46451dc496d4e7c6f025c5814e
Julien Barnier <julien@nozav.org>
2012-06-02 00:40:38 +0200

    Import initial

---
+++ classif-communes.R
@@ -0,0 +1,225 @@
+####Extensions
+
+library(stringr)
+library(ggplot2)
+library(rgrs)
+library(FactoMineR)
+library(grid)
+library(reshape2)
+
+## Chargement des fonctions
+source("fonctions.R")
+
+####Chargement des données et recodages
+
+## Chargement du fichier source brut
+ds <- read.csv("data/source.csv")
+
+## Renommage des variables de base
+d <- data.frame(nom=ds$Libellé.de.la.commune,dpt=ds$Code.du.département,com=ds$Code.de.la.commune)
+  
+## Padding des identifiants
+d$dpt <- str_pad(d$dpt, width=2, pad="0", side="left")
+d$com <- str_pad(d$com, width=3, pad="0", side="left")
+## Identifiant INSEE
+d$id.com <- paste(d$dpt, d$com, sep="")
+## Rownames
+rownames(d) <- paste0(d$dpt,".",d$nom)
+  
+## Import et renommages des variables de vote
+d$inscr <- ds$Inscrits
+d$voix.abst <- ds$Abstentions
+d$votants <- ds$Votants
+d$voix.blancs <- ds$Blancs.et.nuls
+d$voix.joly <- ds$Voix
+d$voix.lepen <- ds$Voix.1
+d$voix.sarkozy <- ds$Voix.2
+d$voix.melenchon <- ds$Voix.3
+d$voix.poutou <- ds$Voix.4
+d$voix.arthaud <- ds$Voix.5
+d$voix.cheminade <- ds$Voix.6
+d$voix.bayrou <- ds$Voix.7
+d$voix.dupont_aignan <- ds$Voix.8
+d$voix.hollande <- ds$Voix.9
+  
+## Suppression des Collectivités d'outre-mer et français de l'étranger
+## on ne garde que les DOM
+d <- d[!(d$dpt %in% c("ZN","ZP","ZS","ZW","ZX","ZZ")),]  
+
+## Suppression des 3 communes annulées par le conseil constitutionnel
+d <- d[!(rownames(d) %in% c("10.Pont-sur-Seine","31.Bourg-d'Oueil","43.Lissac")),]
+
+## Calcul des variables de pourcentage pour chaque candidat
+d$abst <- d$voix.abst / d$inscr * 100
+noms <- c("blancs","joly","lepen","sarkozy","melenchon","poutou",
+          "arthaud","cheminade","bayrou","dupont_aignan","hollande") 
+for (nom in noms) {
+  d[,nom] <- d[,paste0("voix.",nom)] / d$votants * 100
+}
+
+
+
+####Classification sur données brutes
+
+
+#####Calcul de la classification
+
+vars <- c("abst","blancs","joly","lepen","sarkozy","melenchon","poutou",
+          "arthaud","cheminade","bayrou","dupont_aignan","hollande")
+tmp.brut <- d[,vars]
+
+save(tmp.brut, file="ec2/in/tmp_brut.rda")
+
+## Calcul de la classification, exécuté sur une instance EC2 a
+## avec 32Go de RAM. Voir ec2/travail.R
+
+##library(flashClust)
+##load("in/tmp_brut.rda")
+##dist.brut <- dist(tmp.brut)
+##hc.brut.ward <- hclust(dist.brut,method="ward")
+##save(hc.brut.ward, file="out/hc_brut_ward.rda")
+
+## Chargement du résultat du calcul
+load("ec2/out/hc_brut_ward.rda")
+
+## Affichage du dendrogramme
+plclust(hc.brut.ward,labels=FALSE,hang=0)
+
+#####Choix du nombre de classes
+
+## Le code suivant génère des graphiques décrivant les écarts à la moyenne
+## pour les différentes variables pour chaque groupe, pour l'ensemble des classifications
+## comprenant entre 2 et 30 groupes. Étudier tour à tour chacune de ces classifications
+## peut aider à choisir le nombre de classes final retenu
+##
+## for (nb.classes.brut in 2:30) {
+##   groupes.brut <- factor(cutree(hc.brut.ward, k=nb.classes.brut))
+##   table(groupes.brut)
+##   m.brut <- diffmatrix(vars, groupes.brut, seuil=0.001)
+##   pdf(file=paste("tmp/diffmatrix_brut_",nb.classes.brut,".pdf",sep=""))
+##   print(diffmatrix.plot(m.brut, seuil.diff=0.5, title=paste("Normée", "Ward", nb.classes.brut, sep=" - ")))
+##   dev.off()
+## }
+
+
+## On conserve 9 classes
+nb.classes.brut <- 9
+
+## Calcul des groupes
+groupes.brut <- factor(cutree(hc.brut.ward, k=nb.classes.brut))
+## Effectifs
+table(groupes.brut)
+  
+
+
+#####Description des classes
+
+vars <- c("lepen","sarkozy","dupont_aignan","bayrou","hollande",
+          "joly","melenchon","poutou","arthaud","cheminade",
+          "blancs","abst")
+labels <- c("Le Pen","Sarkozy","Dupont-Aignan","Bayrou","Hollande",
+            "Joly","Mélenchon","Poutou","Arthaud","Cheminade",
+            "Blancs","Abstention")
+
+## Graphique comparant les moyennes pour chaque variable et pour chaque
+## groupe. Seuls les écarts supérieurs à 0.5 points en valeur absolue
+## et significatif au seuil de 0.001 sont affichés.
+
+m.brut <- diffmatrix(vars, groupes.brut, seuil=0.001, levels=vars, labels=labels)
+
+diffmatrix.plot(m.brut, seuil.diff=0.5,
+                levels=vars, labels=labels,
+                title=paste("Brute", "Ward", nb.classes.brut, sep=" - "))
+
+## Répartition du vote Le Pen dans les différents groupes
+grplot("lepen", groupes.brut)
+
+## Calcul des variables centrées (on leur soustrait
+## leur moyenne nationale)
+dv <- d[,vars]
+moyennes <- apply(dv, 2, mean)
+dv <- sweep(dv, 2, moyennes)
+dv$groupes <- groupes.brut
+## Passage en format "long"
+dvm <- melt(dv)
+
+## Export d'un graphe de comparaison de densités pour chaque groupe
+for (groupe in 1:nb.classes.brut) {
+  png(file=paste("out/densites_groupe",groupe,".png",sep=""))
+  print(groupes.density(dvm, groupe=groupe))
+  dev.off()
+}
+
+
+####Export pour Fusion tables
+
+## On charge le fichier rgc.rda, qui contient un data frame
+## nommé com.points contenant des données pour les communes
+## de France tirées du RGC.
+load(file = "data/rgc.rda") ## com.points
+  
+tmp.rgc <- rgc[,c("id","long","lat")]
+tmp.d <- d[,c("nom","id.com", vars)]
+tmp.d$groupes <- groupes.brut
+tmp.d[,vars] <- round(tmp.d[,vars],1)
+
+geo <- merge(tmp.rgc, tmp.d, by.x="id", by.y="id.com", all.x=FALSE, all.y=FALSE)
+
+## Noms des marqueurs pour les différents groupes
+geo$marker[geo$groupes==1] <- "ltblu_circle"
+geo$marker[geo$groupes==2] <- "purple_circle"
+geo$marker[geo$groupes==3] <- "grn_circle"
+geo$marker[geo$groupes==4] <- "red_circle"
+geo$marker[geo$groupes==5] <- "orange_circle"
+geo$marker[geo$groupes==6] <- "pink_circle"
+geo$marker[geo$groupes==7] <- "ylw_circle"
+geo$marker[geo$groupes==8] <- "blu_circle"
+geo$marker[geo$groupes==9] <- "wht_circle"
+
+couleurs <- c("#55D7D7",
+              "#7E55FC",
+              "#00E93C",
+              "#FC6355",
+              "#FF9C00",
+              "#E14D9C",
+              "#FCF457",
+              "#5781FC",
+              "#BBBBBB")
+              
+
+## Export CSV pour import dans Fusion table
+write.csv(geo, file="out/export_fusion_table.csv")
+  
+## Pour mémoire, code HTML pour l'affichage des info-window
+## dans Google Maps :
+##
+## <div class='googft-info-window' style='font-family: sans-serif'>
+## <h3>{nom}</h3>
+## <p><b>Groupe {groupes}</b></p>
+## <table>
+## <tr><td>Le Pen</td><td>{lepen}%</td></tr>
+## <tr><td>Sarkozy</td><td>{sarkozy}%</td></tr>
+## <tr><td>Dupont-Aignan</td><td>{dupont_aignan}%</td></tr>
+## <tr><td>Bayrou</td><td>{bayrou}%</td></tr>
+## <tr><td>Hollande</td><td>{hollande}%</td></tr>
+## <tr><td>Joly</td><td>{joly}%</td></tr>
+## <tr><td>Mélenchon</td><td>{melenchon}%</td></tr>
+## <tr><td>Poutou</td><td>{poutou}%</td></tr>
+## <tr><td>Arthaud</td><td>{arthaud}%</td></tr>
+## <tr><td>Cheminade</td><td>{cheminade}%</td></tr>
+## <tr><td>Blancs et nuls</td><td>{blancs}%</td></tr>
+## <tr><td>Abstention</td><td>{abst}%</td></tr>
+## </table>
+## </div>
+
+
+####Cartographie
+
+## Export des cartes de répartition des communes des différents groupes
+
+for (groupe in 1:nb.classes.brut) {
+  png(file=paste("out/carte_groupe",groupe,".png",sep=""))
+  print(groupe.carte(geo, groupe=groupe, couleurs))
+  dev.off()
+}
+
---
+++ fonctions.R
@@ -0,0 +1,109 @@
+####Fonctions
+
+
+## Affiche la répartition des votes pour les différents groupes
+## pour un candidat sous forme de boxplot et de violin plot
+
+grplot <- function (varname, vgroupes, title="") {
+  tmp <- data.frame(groupes=vgroupes, var=d[,varname])
+  ggplot(data=tmp, aes(x=groupes, y=var))  +
+    geom_hline(yintercept=median(tmp$var), colour="red") +
+    geom_violin(aes(fill=groupes, colour=groupes)) +
+    geom_boxplot(aes(fill=groupes), outlier.colour=NA, width=0.2) +
+    opts(title=varname, legend.position="none") + xlab("Groupe") + ylab("Pourcentage") +
+    scale_fill_brewer(palette="Set3") + scale_colour_brewer(palette="Set3")
+}
+
+
+## Pour chaque groupe, calcul l'écart entre la moyenne de var
+## dans ce groupe et la moyenne globale de var, fait un test t
+## et conserve le résultat seulement si sa significativité est
+## inférieure à seuil
+
+difftests <- function(var, groupes, seuil=0.05) {
+  v <- sapply(levels(groupes), function(i) {
+    var.values <- var[groupes==i]
+    diff.mean <- round(mean(var.values)-mean(var),2)
+    if (length(var.values)<2) p.mean <- 0
+    else p.mean <- t.test(var ~ groupes==i)$p.value
+    if (p.mean > seuil) diff.mean <- NA
+    return(diff.mean) })        
+  names(v) <- levels(groupes)
+  v
+}
+
+## Construit une matrice en agrégeant les difftests pour chaque
+## variable de vars
+
+diffmatrix <- function(vars, groupes, seuil=0.05, levels, labels) {
+  m <- sapply(vars, function(name) {
+    difftests(d[,name], groupes, seuil=seuil)
+    })
+  t(m)
+}
+
+## Représentation graphique d'une matrice calculée avec diffmatrix
+
+diffmatrix.plot <- function(m, seuil.diff=0, levels, labels, title=NULL) {
+  mm <- melt(m)
+  mm$Var1 <- factor(mm$Var1, levels=vars, labels=labels)
+  mm$value[abs(mm$value) < seuil.diff] <- NA
+  ggplot(data=subset(mm, !is.na(value))) +
+    geom_tile(aes(x=Var1, y=Var2,fill=value), colour="white") +
+    scale_fill_gradient2(low="blue", mid="white", high="red") +
+    geom_text(aes(x=Var1, y=Var2,label=value), size=2.5) +
+    scale_y_continuous(breaks=1:max(mm$Var2)) +
+    opts(axis.text.x=theme_text(angle=45,hjust=1,vjust=1)) +
+    xlab("") + ylab("Groupe") + opts(title=title)
+}
+
+## Autre représentation graphique d'une matrice calculée avec diffmatrix
+## (inutilisée)
+
+diffmatrix.barplot <- function(m) {
+  mm <- melt(m)
+  mm$Var1 <- factor(mm$Var1, levels=vars, labels=labels)
+  qplot(data=mm,x=Var1, y=value, geom="bar", stat="identity") + 
+    geom_hline(yintercept=0, colour="grey80") + 
+    facet_grid(Var2~.)
+}
+
+## Affiche la répartition de la densité pour chaque variable
+## pour un groupe donné
+
+groupes.density <- function(df, groupe) {
+  tmp <- df[df$groupes==groupe,]
+  tmp$variable <- factor(tmp$variable, levels=vars, labels=labels)
+  ggplot(data=tmp) +
+    geom_density(aes(x=value, fill=variable, color=variable, alpha=0.6)) +
+    geom_vline(xintercept=0, linetype=2) +
+    facet_grid(variable~., scales="free") +
+    scale_x_continuous(limits=c(-23,23), name="Écart à la moyenne nationale") +
+    scale_y_continuous(breaks=NULL, name="") +
+    opts(legend.position="none", strip.text.y=theme_text()) +
+    opts(title=paste("Groupe", groupe, sep=" "), plot.margin=unit(c(2, 1, 2, 0), "lines")) +
+    opts(axis.title.x=theme_text(vjust=-1), plot.title=theme_text(vjust=2, size=14))
+}
+
+
+## Affiche une carte de France avec la position des communes
+## du groupe selon la couleur des marqueurs de Google, et
+## superpose des lignes de niveau de densité.
+
+
+groupe.carte <- function(geo, groupe, couleurs) {
+tmp <- geo[geo$groupes==groupe,]
+ggplot(data=tmp, aes(long,lat)) +
+  borders("france", size=0.3, colour="grey30") +
+  geom_point(color=couleurs[groupe],size=1.5) +
+  geom_density2d(color="black") +
+  theme_bw() +
+  coord_map() +
+  opts(title=paste("Groupe",groupe,sep=" "),
+       legend.position="none",
+       axis.ticks = theme_blank(), 
+       axis.title.y = theme_blank(), 
+       axis.text.y =  theme_blank(),
+       axis.title.x = theme_blank(), 
+       axis.text.x =  theme_blank()) 
+}
